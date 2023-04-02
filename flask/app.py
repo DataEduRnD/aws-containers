dbe7db91a1c65c00da7ebb13117000cf7d7ad684
tothevic <tothevic@gmail.com>
2023-04-03 00:05:53 +0900

    final result

--- flask/app.py
+++ flask/app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+import socket
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello, World"
+    return f"Cotainer ID: {socket.gethostname()}"
+    # return "Hello, app.py"
 
 if __name__ == "__main__":
-   app.run(host="0.0.0.0", port=5000)
+   app.run(host="0.0.0.0", debug = True, port = 5000)
    
